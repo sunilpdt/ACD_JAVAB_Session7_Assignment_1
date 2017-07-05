2ad25b114130a8126e47410604f5d1035943966c
Sunil <sunilpdt@gmail.com>
2017-07-05 10:09:30 +0530

    Committing 7.1

---
+++ ACD_JAVAB_Session7_Assignment_1/src/com/acadgild/IntToString.java
@@ -0,0 +1,12 @@
+package com.acadgild;
+
+public class IntToString {
+
+	public static void main(String[] args) {
+		
+		int number=300;
+		System.out.println("Integer to String Conversion:"+String.valueOf(number));
+
+	}
+
+}
